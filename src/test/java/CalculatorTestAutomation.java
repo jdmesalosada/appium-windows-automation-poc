ba39655e18e88ed8567ae839fbfef2e718bd34bf
Julian <jdmesalosada@gmail.com>
2024-05-03 19:00:48 -0500

    added sum test

--- src/test/java/CalculatorTestAutomation.java
+++ src/test/java/CalculatorTestAutomation.java
@@ -1,6 +1,10 @@
+
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.windows.WindowsDriver;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.IOException;
@@ -15,7 +19,6 @@ public class CalculatorTestAutomation {
         DesiredCapabilities caps = new DesiredCapabilities();
 
         caps.setCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
-
         caps.setCapability("platformName", "windows");
         caps.setCapability("automationName", "windows");
 
@@ -30,8 +33,33 @@ public class CalculatorTestAutomation {
     }
 
     @Test
-    public void sumTest(){
+    public void sumTest() throws InterruptedException {
+        //Thread.sleep(3000);
 
+        WebElement maximize = driver.findElement(AppiumBy.accessibilityId("Maximize"));
+        maximize.click();
+
+        Thread.sleep(3000);
     }
 
+    @Test
+    public void sumTest2() throws InterruptedException {
+        WebElement num2 = driver.findElement(AppiumBy.accessibilityId("num2Button"));
+        num2.click();
+
+        WebElement plusButton = driver.findElement(AppiumBy.accessibilityId("plusButton"));
+        plusButton.click();
+
+        num2.click();
+
+        WebElement equalButton = driver.findElement(AppiumBy.accessibilityId("equalButton"));
+        equalButton.click();
+
+        WebElement calculatorResults = driver.findElement(AppiumBy.accessibilityId("CalculatorResults"));
+        String resultText = calculatorResults.getText();
+
+        Assertions.assertEquals("Se muestra 4", resultText);
+
+        Thread.sleep(3000);
+    }
 }
